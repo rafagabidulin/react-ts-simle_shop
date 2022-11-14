983a8da9dbe2d8b49a89f35404cf994cd337c1ad
rafagabidulin <rafagabidulin@gmail.com>
2022-11-14 23:25:21 +0300

    added tailwind, loading data and first component

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import axios, { AxiosError } from 'axios';
+import React, { useState, useEffect } from 'react';
+import { IProduct } from '../models';
+import { Product } from './components/Product';
+// import { products } from './data/products';
+
+function App() {
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  async function fetchProducts() {
+    try {
+      setError('');
+      setLoading(true);
+      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5');
+      setProducts(response.data);
+      setLoading(false);
+    } catch (e: unknown) {
+      const error = e as AxiosError;
+      setLoading(false);
+      setError(error.message);
+    }
+
+  };
+
+  useEffect(() => {
+    fetchProducts()
+  }, []);
+
+  return (
+    <div className='container mx-auto max-w-2xl pt-5'>
+      {loading && <p className='text-center'>Loading...</p>}
+      {error && <p className='text-center text-red-600'>{ error }</p>}
+      { products.map(product =>
+        <Product product={product} key={product.id} />
+      )};
+    </div>
+  );
+}
+
+export default App;
